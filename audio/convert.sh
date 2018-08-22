ea7914c2cf19f4fc37c84c61fa554b5bbdc2d45f
karthik bharadwaj <kbharadwaj@lyft.com>
2018-08-22 15:01:25 -0700

    audio

---
+++ audio/convert.sh
@@ -0,0 +1,19 @@
+sox -r 16000 -c 1 -e signed -c 1 -e signed -b 16 channel_0.raw channel_0.wav
+sox -r 16000 -c 1 -e signed -c 1 -e signed -b 16 channel_1.raw channel_1.wav
+sox -r 16000 -c 1 -e signed -c 1 -e signed -b 16 channel_2.raw channel_2.wav
+sox -r 16000 -c 1 -e signed -c 1 -e signed -b 16 channel_3.raw channel_3.wav
+sox -r 16000 -c 1 -e signed -c 1 -e signed -b 16 channel_4.raw channel_4.wav
+sox -r 16000 -c 1 -e signed -c 1 -e signed -b 16 channel_5.raw channel_5.wav
+sox -r 16000 -c 1 -e signed -c 1 -e signed -b 16 channel_6.raw channel_6.wav
+sox -r 16000 -c 1 -e signed -c 1 -e signed -b 16 channel_7.raw channel_7.wav
+sox -r 16000 -c 1 -e signed -c 1 -e signed -b 16 channel_8.raw channel_8.wav
+
+#rm -f channel_0.raw
+#rm -f channel_1.raw
+#rm -f channel_2.raw
+#rm -f channel_3.raw
+#rm -f channel_4.raw
+#rm -f channel_5.raw
+#rm -f channel_6.raw
+#rm -f channel_7.raw
+#rm -f channel_8.raw
---
+++ audio/test/singlechannel.py
@@ -0,0 +1,65 @@
+'''
+Simple UDP listen test, with the target being the driver
+
+Steps to run: 
+    * Set IP of host to 192.168.1.11
+    * Target IP should be configured to 192.168.1.10
+    * Host Port: 8001
+    * Target Port: 8000
+	* Run this script
+    * Reset the target, host should now be listening for packets
+	* Script breaks out of forever loop if data is not what's expected
+    * Set DEBUG = True for packet count
+'''
+
+import socket
+import time
+import subprocess
+
+# TurnMeOn
+DEBUG = True
+
+PC_IP = "10.112.101.197"
+PC_PORT = 55555
+
+fd_list = []
+
+def init():
+    f = open('channel_{}_test.raw'.format(0), 'wb')
+    fd_list.append(f)
+
+
+def processData(data):
+    channel = 0
+
+    fd = fd_list[channel]
+    fd.write(data)
+
+
+def main():
+    sock = socket.socket(socket.AF_INET, # Internet
+                        socket.SOCK_DGRAM) # UDP
+    sock.bind((PC_IP, PC_PORT))
+
+    counter = 0
+
+    init()
+
+    while True:
+        rxData, addr = sock.recvfrom(32768)
+        counter = counter + 1
+
+        processData(rxData)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("closing files")
+        for fd in fd_list:
+            fd.close()
+
+        print("converting raw to wav")
+        subprocess.call("./convert.sh", shell=True)
+        print("Done")
+        print("")
